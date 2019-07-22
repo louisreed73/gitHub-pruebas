5cde96b4881499ab7db828ce75b1c110eeb10541
louisreed73 <luis.sa1973@gmail.com>
2019-07-22 11:11:39 +0200

    cambios lunes 22/07

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,35 @@ import '../css/estilo.scss';
 import '../imgs/coches/img2.png';
 import '../imgs/img1.png';
 
+// let lista=[...document.querySelectorAll("div[class|='panel']")];
+
+let {log}=console;
+
+// lista.forEach(item=>item.addEventListener("click",function(){
+
+
+//     log(this);
+// }));
+
+// log(lista);
+
+let padre=document.querySelector(".parent");
+
+padre.addEventListener("click",function(e){
+    log("Hola!!!",this,e.target);
+
+    let hijos=[...this.children];
+
+    log(hijos,Array.isArray(hijos));
+
+    let este=hijos.indexOf(e.target);
+
+    log(este)
+    
+})
+
+
+
 
 
 
