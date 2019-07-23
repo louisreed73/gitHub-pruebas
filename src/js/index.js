0c382eb45ebdccece19792cfd07651766f66ab8c
louisreed73 <luis.sa1973@gmail.com>
2019-07-23 12:08:55 +0200

    ultimos cambios martes 23/07

--- src/js/index.js
+++ src/js/index.js
@@ -38,7 +38,63 @@ padre.addEventListener("click",function(e){
 
     log(este)
     
-})
+});
+let num=0;
+
+let circulos = [...document.querySelectorAll("[class|='circulo']")];
+
+
+circulos.forEach(item=>item.addEventListener("transitionend",(e)=>{
+    // log(e.target,e.propertyName);
+
+
+        e.target.classList.remove("este");
+
+    // circulos[num].classList.remove("este");
+
+})) 
+
+
+
+
+log(circulos)
+
+function prueba(num) {
+    
+    log(`Hola desde la funcion prueba ${num}  !!!!!!!!`);
+    // circulos[num].classList.add("este");
+    // log(num)
+    circulos.forEach((item,ind)=>{
+        setTimeout(function(){
+            item.classList.add("este")
+        },(500/3)*ind)
+        // item.classList.add("")
+    })
+    setTimeout(prueba,1000)
+    
+
+}
+
+
+
+// let _arrayT=[prueba1,prueba2,prueba3]
+
+
+function llamada(callb) {
+    let funcion=new Function(callb)
+    log(funcion)
+        funcion();
+};
+
+
+
+prueba();
+
+
+
+// (`prueba${num}`).call(null)
+
+// log(window)
 
 
 
